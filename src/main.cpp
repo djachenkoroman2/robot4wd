f31ef3519e3afd6b404edb49ef2c8c342a30f43f
djachenkoroman2 <djachenko.roman2@gmail.com>
2026-01-26 19:16:27 +0300

    Win

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,8 @@
 #include"PID.h"
 
 #define SERIALSPEED 9600
-#define DT 100
-
-#define DT2 50
+#define DT 150
+#define DT2 500
 #define SPD_TARGET 1500
 
 volatile unsigned int rot = 0;
@@ -17,29 +16,30 @@ unsigned long lastCalcTime = 0;
 unsigned long lastCalcTime2 = 0;
 unsigned long currentTime = 0;
 unsigned long currentTime2 = 0;
-float filteredSpeed = 0;
 
-SimpleKalmanFilter kalmanFilter(0.01, 0.7, 0); // Q=0.01, R=0.1
-MovingAverageFilter MAFilter(30); 
+// float filteredSpeed = 0;
+
+// SimpleKalmanFilter kalmanFilter(0.01, 0.7, 0); // Q=0.01, R=0.1
+MovingAverageFilter MAFilter(10); 
 
 AbstractFilter& filter1=MAFilter; 
-AbstractFilter& filter2=kalmanFilter; 
+// AbstractFilter& filter2=kalmanFilter; 
 
 // PID pidController(20.0, 0.0, 0.0, DT2/1000); // Kp=2, Ki=5, Kd=1, время дискретизации 100 мс
-float Kp, Ki, Kd;
-float integral, pError;
+// float Kp, Ki, Kd;
+// float integral, pError;
 
 
-float computePID(float setpoint, float input1, float dt) {
-    float input;
-    if (isnan(input1)) input=0; else input = input1;
-    float error = setpoint - input;
-    // float derivative = (error - pError) / dt;
-    pError = error;
-    integral += error * dt;
-    // return error * Kp + integral * Ki + derivative * Kd;
-    return error * Kp + integral * Ki;
-}
+// float computePID(float setpoint, float input1, float dt) {
+//     float input;
+//     if (isnan(input1)) input=0; else input = input1;
+//     float error = setpoint - input;
+//     // float derivative = (error - pError) / dt;
+//     pError = error;
+//     integral += error * dt;
+//     // return error * Kp + integral * Ki + derivative * Kd;
+//     return error * Kp + integral * Ki;
+// }
 
 
 void detect() {
@@ -51,26 +51,31 @@ void setup() {
     pinMode( PIN_MOTORDRV1_ENA, OUTPUT );
     pinMode( PIN_MOTORDRV1_IN1, OUTPUT );
     pinMode( PIN_MOTORDRV1_IN2, OUTPUT );
+    pinMode(PIN_SPD_SENSOR_1, INPUT_PULLUP);
 
     Serial.begin(SERIALSPEED); // Увеличим скорость для отладки
-    
-    pinMode(PIN_SPD_SENSOR_1, INPUT_PULLUP);
+
     attachInterrupt(digitalPinToInterrupt(PIN_SPD_SENSOR_1), detect, RISING);
 
     // Настройка ПИД-регулятора
     // pidController.setTarget(SPD_TARGET);
     // pidController.setOutputLimits(0, 255);  // Для ШИМ
 
-    Kp = 2;
-    Ki = 5;
-    Kd = 0;
-    pError = 0;
-    integral = 0;
+    // Kp = 2;
+    // Ki = 5;
+    // Kd = 0;
+    // pError = 0;
+    // integral = 0;
 
     lastCalcTime2=lastCalcTime = millis();
 }
 
 void loop() {
+
+    // Управление нагревателем через ШИМ
+    analogWrite(PIN_MOTORDRV1_ENA, 130);
+    digitalWrite( PIN_MOTORDRV1_IN1, HIGH );
+    digitalWrite( PIN_MOTORDRV1_IN2, LOW );
 
     currentTime = millis();
     if (currentTime - lastCalcTime >= DT) 
@@ -85,13 +90,12 @@ void loop() {
         if (lastCalcTime > 0) {
             unsigned long timeDiff = currentTime - lastCalcTime;
             if (timeDiff > 0 && rotations > 0) {
-                speedRPM = (rotations * 60000UL) / timeDiff;
+                // filteredSpeed = filter2.update(filter1.update(rotations / timeDiff));
+                speedRPM = int(filter1.update(rotations));
+                
             } else {
                 speedRPM = 0;
             }
-            
-            // Применяем фильтр 
-            filteredSpeed = filter2.update(filter1.update(speedRPM));
         }
         
         lastCalcTime = currentTime;
@@ -102,21 +106,14 @@ void loop() {
     {
         // Вычисление ПИД
         // float output = pidController.compute(filteredSpeed);
-        float output = computePID(SPD_TARGET, speedRPM, DT2/1000);
-        int val = map(output,-3600,3600,0,255);
-        // Управление нагревателем через ШИМ
-        analogWrite(PIN_MOTORDRV1_ENA, val);
-        // analogWrite(PIN_MOTORDRV1_ENA, 255);
-        digitalWrite( PIN_MOTORDRV1_IN1, HIGH );
-        digitalWrite( PIN_MOTORDRV1_IN2, LOW );
+        // float output = computePID(SPD_TARGET, speedRPM, DT2/1000);
+        // int val = map(output,0,3600,0,255);
 
-        Serial.print(speedRPM);
-        Serial.print(" ");
-        Serial.print(SPD_TARGET);
-        Serial.print(" ");
-        Serial.print(filteredSpeed, 1); 
-        Serial.print(" ");
-        Serial.println(output, 1); 
+        Serial.println(speedRPM);
+        // Serial.print(" ");
+        // Serial.print(SPD_TARGET);
+        // Serial.print(" ");
+        // Serial.println(output, 1); 
         lastCalcTime2 = currentTime2;
     }
     delay(1);
